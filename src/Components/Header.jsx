d82a2dad5273839120a2b430f62552e5b58a1d98
Ikjyot-Kohli <ikjyotkohli@gmail.com>
2026-05-28 14:53:41 +0530

    Updated project code

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="w-full bg-black/40 backdrop-blur-md text-white text-center py-4 border-t border-white/20">
       <h1 className="text-3xl font-bold">Weather Dashboard</h1>
       <p className="text-sm text-black-500">
-        Real-time weather & 5-day forecast
+        Real-time Weather & 6-day Forecast
       </p>
     </header>
   );
